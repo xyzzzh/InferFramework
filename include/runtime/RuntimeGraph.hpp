155b8e88ff83c1c16f43ba32f91877737beee65e
xyzzzh <972395684@qq.com>
2024-04-01 16:19:00 +0800

    feat: 实现计算图类

--- include/runtime/RuntimeGraph.hpp
+++ include/runtime/RuntimeGraph.hpp
@@ -15,9 +15,59 @@
 class RuntimeGraph {
 public:
     // 初始化计算图
+    RuntimeGraph(std::string param_path, std::string bin_path);
+
+    // 构建计算图
+    void build(const std::string &input_name, const std::string &output_name);
+
+    // 设置结构文件
+    void set_param_path(const std::string& param_path);
+
+    // 设置权重文件
+    void set_bin_path(const std::string& bin_path);
+
+    // 返回结构文件
+    const std::string &param_path() const;
+
+    // 返回权重文件
+    const std::string &bin_path() const;
+
+    // 计算图的初始化
+    bool init();
+
+    const std::vector<std::shared_ptr<RuntimeOperator>> &operators() const;
 
 private:
+    // 初始化计算图节点中的输入操作数
+    static void init_graph_operators_input(
+            const std::vector<pnnx::Operand *> &inputs,
+            const std::shared_ptr<RuntimeOperator> &runtime_operator);
 
+    // 初始化计算图节点中的输出操作数
+    static void init_graph_operators_output(
+            const std::vector<pnnx::Operand *> &outputs,
+            const std::shared_ptr<RuntimeOperator> &runtime_operator);
+
+    // 初始化计算图中的节点属性
+    static void
+    init_graph_attrs(const std::map<std::string, pnnx::Attribute> &attrs,
+                   const std::shared_ptr<RuntimeOperator> &runtime_operator);
+
+    // 初始化计算图中的节点参数
+    static void
+    init_graph_params(const std::map<std::string, pnnx::Parameter> &params,
+                    const std::shared_ptr<RuntimeOperator> &runtime_operator);
+
+private:
+    std::string m_input_name;  /// 计算图输入节点的名称
+    std::string m_output_name; /// 计算图输出节点的名称
+    std::string m_param_path;  /// 计算图的结构文件
+    std::string m_bin_path;    /// 计算图的权重文件
+
+    std::vector<std::shared_ptr<RuntimeOperator>> m_operators;
+    std::map<std::string, std::shared_ptr<RuntimeOperator>> m_operators_maps;
+
+    std::unique_ptr<pnnx::Graph> m_graph; /// pnnx的graph
 };
 
 
--- source/runtime/RuntimeGraph.cpp
+++ source/runtime/RuntimeGraph.cpp
@@ -3,3 +3,240 @@
 //
 
 #include "runtime/RuntimeGraph.hpp"
+
+RuntimeGraph::RuntimeGraph(std::string param_path, std::string bin_path) {
+    this->m_param_path = std::move(param_path);
+    this->m_bin_path = std::move(bin_path);
+}
+
+void RuntimeGraph::build(const std::string &input_name, const std::string &output_name) {
+
+}
+
+void RuntimeGraph::set_param_path(const std::string &param_path) {
+    this->m_param_path = param_path;
+}
+
+void RuntimeGraph::set_bin_path(const std::string &bin_path) {
+    this->m_bin_path = bin_path;
+}
+
+const std::string &RuntimeGraph::param_path() const {
+    return this->m_param_path;
+}
+
+// 返回权重文件
+const std::string &RuntimeGraph::bin_path() const {
+    return this->m_bin_path;
+}
+
+// 计算图的初始化
+bool RuntimeGraph::init() {
+    if (this->m_bin_path.empty() || this->m_param_path.empty()) {
+        LOG(ERROR) << "The bin path or param path is empty";
+        return false;
+    }
+
+    this->m_graph = std::make_unique<pnnx::Graph>();
+
+    int load_res = this->m_graph->load(this->m_param_path, this->m_bin_path);
+    if (load_res != 0) {
+        LOG(ERROR) << "Can not find the param path or bin path: " << this->m_param_path
+                   << " " << this->m_bin_path;
+        return false;
+    }
+    std::vector<pnnx::Operator *> operators = this->m_graph->ops;
+    if (operators.empty()) {
+        LOG(ERROR) << "Can not read the layers' define";
+        return false;
+    }
+
+    this->m_operators.clear();
+    this->m_operators_maps.clear();
+
+    for (const auto &op: operators) {
+        if (op == nullptr) {
+            LOG(ERROR) << "Meet the empty node";
+            continue;
+        } else {
+            std::shared_ptr<RuntimeOperator> runtime_operator = std::make_shared<RuntimeOperator>();
+            runtime_operator->m_name = op->name;
+            runtime_operator->m_type = op->type;
+
+            // 初始化算子中的input
+            const auto &inputs = op->inputs;
+            if (!inputs.empty()) {
+                init_graph_operators_input(inputs, runtime_operator);
+            }
+
+            // 记录输出operand中的名称
+            const auto &outputs = op->outputs;
+            if (!outputs.empty()) {
+                init_graph_operators_output(outputs, runtime_operator);
+            }
+
+            // 初始化算子中的attribute(权重)
+            const auto &attrs = op->attrs;
+            if (!attrs.empty()) {
+                init_graph_attrs(attrs, runtime_operator);
+            }
+
+            // 初始化算子中的parameter
+            const auto &params = op->params;
+            if (!params.empty()) {
+                init_graph_params(params, runtime_operator);
+            }
+
+            this->m_operators.push_back(runtime_operator);
+            this->m_operators_maps.insert({runtime_operator->m_name, runtime_operator});
+        }
+    }
+    return true;
+}
+
+const std::vector<std::shared_ptr<RuntimeOperator>> &RuntimeGraph::operators() const {
+    return this->m_operators;
+}
+
+// 初始化计算图节点中的输入操作数
+void RuntimeGraph::init_graph_operators_input(
+        const std::vector<pnnx::Operand *> &inputs,
+        const std::shared_ptr<RuntimeOperator> &runtime_operator) {
+    for (const auto input: inputs) {
+        if (input == nullptr) {
+            continue;
+        }
+
+        const pnnx::Operator *producer = input->producer;
+        std::shared_ptr<RuntimeOperand> runtime_operand = std::make_shared<RuntimeOperand>();
+        runtime_operand->m_name = producer->name;
+        runtime_operand->m_shapes = std::vector<uint32_t>(input->shape.begin(), input->shape.end());
+
+        // 0=null 1=f32 2=f64 3=f16 4=i32 5=i64 6=i16 7=i8 8=u8 9=bool 10=cp64 11=cp128 12=cp32
+        switch (input->type) {
+            case 1: {
+                runtime_operand->m_type = ERuntimeDataType::ERDT_Float32;
+                break;
+            }
+            case 0: {
+                runtime_operand->m_type = ERuntimeDataType::ERDT_Unknown;
+                break;
+            }
+            default: {
+                LOG(FATAL) << "Unknown input operand type: " << input->type;
+            }
+        }
+        runtime_operator->m_input_operands.insert({producer->name, runtime_operand});
+        runtime_operator->m_input_operands_seq.push_back(runtime_operand);
+    }
+}
+
+// 初始化计算图节点中的输出操作数
+void RuntimeGraph::init_graph_operators_output(
+        const std::vector<pnnx::Operand *> &outputs,
+        const std::shared_ptr<RuntimeOperator> &runtime_operator) {
+    for (const pnnx::Operand *output: outputs) {
+        if (output == nullptr) {
+            continue;
+        }
+        const auto &consumers = output->consumers;
+        for (const auto &c: consumers) {
+            runtime_operator->m_output_names.push_back(c->name);
+        }
+    }
+}
+
+// 初始化计算图中的节点属性
+void
+RuntimeGraph::init_graph_attrs(const std::map<std::string, pnnx::Attribute> &attrs,
+                               const std::shared_ptr<RuntimeOperator> &runtime_operator) {
+    for (const auto &[name, attr]: attrs) {
+        // 0=null 1=f32 2=f64 3=f16 4=i32 5=i64 6=i16 7=i8 8=u8 9=bool
+        switch (attr.type) {
+            case 1: {
+                std::shared_ptr<RuntimeAttribute> runtime_attr = std::make_shared<RuntimeAttribute>();
+                runtime_attr->m_type = ERuntimeDataType::ERDT_Float32;
+                runtime_attr->m_weight_data = attr.data;
+                runtime_attr->m_shapes = std::vector<uint32_t>(attr.shape.begin(), attr.shape.end());
+                runtime_operator->m_attribute.insert({name, runtime_attr});
+                break;
+            }
+            default: {
+                LOG(FATAL) << "Unknown attribute type: " << attr.type;
+            }
+        }
+    }
+}
+
+// 初始化计算图中的节点参数
+void
+RuntimeGraph::init_graph_params(const std::map<std::string, pnnx::Parameter> &params,
+                                const std::shared_ptr<RuntimeOperator> &runtime_operator) {
+    for (const auto& pair : params) {
+        const std::string& name = pair.first;
+        const pnnx::Parameter& parameter = pair.second;
+        const int type = parameter.type;
+        switch (type) {
+            case int(ERuntimeParameterType::ERPT_ParameterUnknown): {
+                RuntimeParameter* runtime_parameter = new RuntimeParameter;
+                runtime_operator->m_params.insert({name, runtime_parameter});
+                break;
+            }
+
+            case int(ERuntimeParameterType::ERPT_ParameterBool): {
+                RuntimeParameterBool* runtime_parameter = new RuntimeParameterBool;
+                runtime_parameter->value = parameter.b;
+                runtime_operator->m_params.insert({name, runtime_parameter});
+                break;
+            }
+
+            case int(ERuntimeParameterType::ERPT_ParameterInt): {
+                RuntimeParameterInt* runtime_parameter = new RuntimeParameterInt;
+                runtime_parameter->value = parameter.i;
+                runtime_operator->m_params.insert({name, runtime_parameter});
+                break;
+            }
+
+            case int(ERuntimeParameterType::ERPT_ParameterFloat): {
+                RuntimeParameterFloat* runtime_parameter = new RuntimeParameterFloat;
+                runtime_parameter->value = parameter.f;
+                runtime_operator->m_params.insert({name, runtime_parameter});
+                break;
+            }
+
+            case int(ERuntimeParameterType::ERPT_ParameterString): {
+                RuntimeParameterString* runtime_parameter = new RuntimeParameterString;
+                runtime_parameter->value = parameter.s;
+                runtime_operator->m_params.insert({name, runtime_parameter});
+                break;
+            }
+
+            case int(ERuntimeParameterType::ERPT_ParameterIntArray): {
+                RuntimeParameterIntArray* runtime_parameter =
+                        new RuntimeParameterIntArray;
+                runtime_parameter->value = parameter.ai;
+                runtime_operator->m_params.insert({name, runtime_parameter});
+                break;
+            }
+
+            case int(ERuntimeParameterType::ERPT_ParameterFloatArray): {
+                RuntimeParameterFloatArray* runtime_parameter =
+                        new RuntimeParameterFloatArray;
+                runtime_parameter->value = parameter.af;
+                runtime_operator->m_params.insert({name, runtime_parameter});
+                break;
+            }
+            case int(ERuntimeParameterType::ERPT_ParameterStringArray): {
+                RuntimeParameterStringArray* runtime_parameter =
+                        new RuntimeParameterStringArray;
+                runtime_parameter->value = parameter.as;
+                runtime_operator->m_params.insert({name, runtime_parameter});
+                break;
+            }
+            default: {
+                LOG(FATAL) << "Unknown parameter type";
+            }
+        }
+    }
+}
+
